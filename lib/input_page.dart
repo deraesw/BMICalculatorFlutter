dc390861e30d34be1144f9ee51823375d55bb05b
deraesw <deraes.wsley@gmail.com>
2019-06-24 17:52:01 +0200

    Add a slider and implement size selection

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -4,10 +4,9 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'card_container_widget.dart';
 import 'gender_widget.dart';
+import 'constants.dart';
 
-const actionButtonHeight = 80.0;
-const containerBackgroundColor = Color(0xFF1D1E33);
-const actionButtonColor = Color(0xFFEB1555);
+enum Gender { male, female }
 
 class InputPage extends StatefulWidget {
   @override
@@ -15,6 +14,21 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Gender selectedGender = Gender.male;
+  int height = 180;
+
+  void _genderSelection(Gender gender) {
+    setState(() {
+      selectedGender = gender;
+    });
+  }
+
+  Color _getBackgroundColor(Gender defineGender) {
+    return selectedGender == defineGender
+        ? containerBackgroundColor
+        : inactiveContainerBackgroundColor;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,13 +36,15 @@ class _InputPageState extends State<InputPage> {
         title: Text('BMI CALCULATOR'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Expanded(
             child: Row(
               children: <Widget>[
                 Expanded(
                   child: CardContainerWidget(
-                    color: containerBackgroundColor,
+                    onTap: () => _genderSelection(Gender.male),
+                    color: _getBackgroundColor(Gender.male),
                     child: GenderWidget(
                       text: 'MALE',
                       icon: FontAwesomeIcons.mars,
@@ -37,7 +53,8 @@ class _InputPageState extends State<InputPage> {
                 ),
                 Expanded(
                   child: CardContainerWidget(
-                    color: containerBackgroundColor,
+                    onTap: () => _genderSelection(Gender.female),
+                    color: _getBackgroundColor(Gender.female),
                     child: GenderWidget(
                       text: 'FEMALE',
                       icon: FontAwesomeIcons.venus,
@@ -50,6 +67,17 @@ class _InputPageState extends State<InputPage> {
           Expanded(
             child: CardContainerWidget(
               color: containerBackgroundColor,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'HEIGHT',
+                    style: labelTextStyle,
+                  ),
+                  buildHeightLabel(),
+                  buildSlider()
+                ],
+              ),
             ),
           ),
           Expanded(
@@ -72,9 +100,53 @@ class _InputPageState extends State<InputPage> {
             margin: EdgeInsets.only(top: 8.0),
             height: actionButtonHeight,
             width: double.infinity,
-            color: actionButtonColor,
+            color: accentColor,
           )
         ],
+      ),
+    );
+  }
+
+  Widget buildHeightLabel() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.baseline,
+      textBaseline: TextBaseline.alphabetic,
+      children: <Widget>[
+        Text(
+          height.toString(),
+          style: labelBigTextStyle,
+        ),
+        SizedBox(
+          width: 8.0,
+        ),
+        Text(
+          'cm',
+          style: labelTextStyle,
+        )
+      ],
+    );
+  }
+
+  Widget buildSlider() {
+    return SliderTheme(
+      data: SliderTheme.of(context).copyWith(
+        activeTrackColor: Colors.white,
+        thumbColor: accentColor,
+        thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15.0),
+        overlayShape: RoundSliderOverlayShape(overlayRadius: 25.0),
+        overlayColor: Color(0x29EB1555)
+      ),
+      child: Slider(
+        onChanged: (double value) {
+          setState(() {
+            height = value.toInt();
+          });
+        },
+        inactiveColor: Color(0xFF8D8E98),
+        value: height.toDouble(),
+        min: 120.0,
+        max: 220.0,
       ),
     );
   }
