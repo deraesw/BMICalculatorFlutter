c6615a9e1a8f1fa8fa3c9025310c178999346fe4
deraesw <deraes.wsley@gmail.com>
2019-06-24 17:51:01 +0200

    Refactoring - Extend the widget with a GestureDetector widget

--- lib/card_container_widget.dart
+++ lib/card_container_widget.dart
@@ -4,24 +4,29 @@ import 'package:flutter/material.dart';
 class CardContainerWidget extends StatelessWidget {
   final Color color;
   final Widget child;
+  final Function onTap;
 
   const CardContainerWidget({
     Key key,
     @required this.color,
-    this.child
+    this.child,
+    this.onTap
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        color: this.color,
-        borderRadius: BorderRadius.circular(
-          10.0,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          color: this.color,
+          borderRadius: BorderRadius.circular(
+            10.0,
+          ),
         ),
+        child: this.child,
       ),
-      child: this.child,
     );
   }
 }
