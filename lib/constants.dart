9d8061c4bce6c3754fd837348a183dfab108088c
deraesw <deraes.wsley@gmail.com>
2019-06-24 17:51:36 +0200

    Create a constants file

---
+++ lib/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const textSize = 18.0;
+
+const actionButtonHeight = 80.0;
+
+const containerBackgroundColor = Color(0xFF1D1E33);
+const inactiveContainerBackgroundColor = Color(0xFF111328);
+const accentColor = Color(0xFFEB1555);
+
+const labelTextStyle = TextStyle(
+  fontSize: textSize,
+  color: const Color(0xFF8D8E98),
+);
+
+const labelBigTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
